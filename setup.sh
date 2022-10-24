545d02d6d4ecbef5733aa17fedeec6755a6d25ef
deeBarber <dee.barber@rover.com>
2022-10-24 16:17:30 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 
-rebase-master() {
-echo 'git checkout master';
-echo 'git pull';
-echo 'git checkout -';
-echo 'git rebase master';
-}
-export -f rebase-master
-
-dee-test() {
-  echo hi friend
-}
-export -f dee-test
-
 echo 'source .codespaces/.persistedshare/dotfiles/functions' >> ~/.profile
